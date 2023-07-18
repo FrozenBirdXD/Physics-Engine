869c663b32571d612faa9064ee6d91bf3e9c386e
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2023-07-18 20:41:10 +0200

    add Camera

---
+++ src/main/java/com/engine/engine/Camera.java
@@ -0,0 +1,37 @@
+package com.engine.engine;
+
+import org.joml.Matrix4f;
+import org.joml.Vector2f;
+import org.joml.Vector3f;
+
+public class Camera {
+    private Matrix4f projectionMatrix, viewMatrix;
+    public Vector2f position;
+
+    public Camera(Vector2f position) {
+        this.position = position;
+        this.projectionMatrix = new Matrix4f();
+        this.viewMatrix = new Matrix4f();
+        adjustProjection();
+    }
+
+    public void adjustProjection() {
+        projectionMatrix.identity();
+        // (0, 1920, 0, 1080, 0.5, 100) as inputs, this means that the camera can "see" any objects that are between x-coordinates 0 and 1920, and y-coordinates 0 and 1080, and z-coordinates 0.5-100
+        projectionMatrix.ortho(0.0f, 32.0f * 40.0f, 0.0f, 32.0f * 21.0f, 0.0f, 100.0f);
+    }
+
+    public Matrix4f getViewMatrix() {
+        // Determines where the camera is and where it is looking at
+        Vector3f cameraFront = new Vector3f(0.0f, 0.0f, -1.0f);
+        Vector3f cameraUp = new Vector3f(0.0f, 1.0f, 0.0f);
+        this.viewMatrix.identity();
+        this.viewMatrix.lookAt(new Vector3f(position.x, position.y, 20.0f),
+                cameraFront.add(position.x, position.y, 0.0f), cameraUp);
+        return this.viewMatrix;
+    }
+
+    public Matrix4f getProjectionMatrix() {
+        return this.projectionMatrix;
+    }
+}
