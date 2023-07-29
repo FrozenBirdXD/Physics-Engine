54d7bf74e648377ef3dd595555ccb8e8a3248384
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2023-07-29 23:49:11 +0200

    initialize components

--- src/main/java/com/engine/engine/Window.java
+++ src/main/java/com/engine/engine/Window.java
@@ -54,10 +54,12 @@ public class Window {
             case 0:
                 currentScene = new WorldEditorScene();
                 currentScene.init();
+                currentScene.start();
                 break;
             case 1:
                 currentScene = new PlaySimulationScene();
                 currentScene.init();
+                currentScene.start();
                 break;
             default:
                 assert false : "Unknown scene '" + newScene + "'";
