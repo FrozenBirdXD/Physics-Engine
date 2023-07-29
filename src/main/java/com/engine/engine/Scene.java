88cf08ab7f9a4f66b64ec76489d66ab1674a7733
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2023-07-29 23:48:52 +0200

    add new methods for game object handling

--- src/main/java/com/engine/engine/Scene.java
+++ src/main/java/com/engine/engine/Scene.java
@@ -1,11 +1,34 @@
 package com.engine.engine;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class Scene {
     protected Camera camera;
+    private boolean isRunning = false;
+    protected List<GameObject> gameObjects = new ArrayList<>();
 
-    public Scene() {}
+    public Scene() {
+    }
 
     public abstract void update(float dt);
 
-    public void init() {}
+    public void init() {
+    }
+
+    public void start() {
+        for (GameObject object : gameObjects) {
+            object.start();
+        }
+        isRunning = true;
+    }
+
+    public void addGameObjectToScene(GameObject object) {
+        if (!isRunning) {
+            gameObjects.add(object);
+        } else {
+            gameObjects.add(object);
+            object.start();
+        }
+    }
 }
