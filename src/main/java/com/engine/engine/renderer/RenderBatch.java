10e788513c103978abe1cee51b352e3aa36b02e8
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2023-10-24 22:02:06 +0200

    create outline for RenderBatch

---
+++ src/main/java/com/engine/engine/renderer/RenderBatch.java
@@ -0,0 +1,30 @@
+package com.engine.engine.renderer;
+
+import com.engine.engine.components.SpriteRenderer;
+
+public class RenderBatch {
+    private Shader shader;
+    private SpriteRenderer[] sprites;
+    private float[] vertices;
+    private int vaoID, vboID;
+
+    public void start() {
+
+    }
+
+    public void addSprite(SpriteRenderer spriteRenderer) {
+
+    }
+
+    private int[] generateIndices() {
+        return new int[0];
+    }
+
+    public void render() {
+
+    }
+
+    private void loadVertexProperties(int index) {
+
+    }
+}
