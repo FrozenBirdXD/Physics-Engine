9f8bd1a6c9b3b62bc50673482a6c5b4ee7327ec1
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2023-10-24 22:32:46 +0200

    create useful variables

--- src/main/java/com/engine/engine/renderer/RenderBatch.java
+++ src/main/java/com/engine/engine/renderer/RenderBatch.java
@@ -3,10 +3,27 @@ package com.engine.engine.renderer;
 import com.engine.engine.components.SpriteRenderer;
 
 public class RenderBatch {
-    private Shader shader;
+    // Vertex
+    // =====
+    // Pos                      Color
+    // float, float,            float, float, float, float
+
+    private Shaders shader;
     private SpriteRenderer[] sprites;
+    private int numSprites;
+    private boolean hasRoom;
     private float[] vertices;
+
     private int vaoID, vboID;
+    private int maxBatchSize;
+
+    private final int POS_SIZE = 2;
+    private final int COLORSIZE = 4;
+
+    private final int POS_OFFSET = 0;
+    private final int COLOR_OFFSET = POS_OFFSET + POS_SIZE * Float.BYTES;
+    private final int VERTEX_SIZE = 6;
+    private final int VERTEX_SIZE_BYTES = VERTEX_SIZE * Float.BYTES;
 
     public void start() {
 
