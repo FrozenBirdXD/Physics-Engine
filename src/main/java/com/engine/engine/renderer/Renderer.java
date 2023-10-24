e2c941dd2068e19604f858dd129ffadf2b585e50
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2023-10-24 22:02:17 +0200

    Create outline for renderer

---
+++ src/main/java/com/engine/engine/renderer/Renderer.java
@@ -0,0 +1,18 @@
+package com.engine.engine.renderer;
+
+import java.util.List;
+
+import com.engine.engine.GameObject;
+
+public class Renderer {
+    private List<RenderBatch> renders;
+
+    public void add(GameObject object) {
+
+    }
+
+    private void render() {
+
+    }
+
+}
