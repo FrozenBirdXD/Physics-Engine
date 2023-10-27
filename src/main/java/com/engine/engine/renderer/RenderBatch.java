dcc98aa5711edadb4ff20de0856b3d1fc585b831
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2023-10-27 23:37:14 +0200

    Create helper function to get indices of element

--- src/main/java/com/engine/engine/renderer/RenderBatch.java
+++ src/main/java/com/engine/engine/renderer/RenderBatch.java
@@ -73,7 +73,26 @@ public class RenderBatch {
 
     private int[] generateIndices() {
         // 6 indices per quad (3 per triangle)
-        return new int[0];
+        int[] elements = new int[6 * maxBatchSize];
+        for (int i = 0; i < maxBatchSize; i++) {
+            loadElementIndices(elements, i);
+        }
+        return elements;
+    }
+
+    private void loadElementIndices(int[] elements, int index) {
+        int offsetArrayIndex = 6 * index;
+        int offset = 4 * index;
+        // 3, 2, 0, 0, 2, 1         7, 6, 4, 4, 6, 5
+        // Create triangle 1
+        elements[offsetArrayIndex] = offset + 3;
+        elements[offsetArrayIndex + 1] = offset + 2; 
+        elements[offsetArrayIndex + 2] = offset + 0; 
+        
+        // Create triangle 2
+        elements[offsetArrayIndex + 3] = offset + 0; 
+        elements[offsetArrayIndex + 4] = offset + 2; 
+        elements[offsetArrayIndex + 5] = offset + 1; 
     }
 
     public void render() {
