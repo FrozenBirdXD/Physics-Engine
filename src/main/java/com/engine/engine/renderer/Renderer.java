89b1619a38e69faad529d71f65003f078ab595fa
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2023-10-29 00:39:05 +0200

    implement renderer class

--- src/main/java/com/engine/engine/renderer/Renderer.java
+++ src/main/java/com/engine/engine/renderer/Renderer.java
@@ -1,18 +1,49 @@
 package com.engine.engine.renderer;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.engine.engine.GameObject;
+import com.engine.engine.components.SpriteRenderer;
 
 public class Renderer {
-    private List<RenderBatch> renders;
+    private final int MAX_BATCH_SIZE = 10000;
+    private List<RenderBatch> batches;
 
-    public void add(GameObject object) {
-
+    public Renderer() {
+        this.batches = new ArrayList<>();
     }
 
-    private void render() {
+    public void add(GameObject object) {
+        SpriteRenderer sprite = object.getComponent(SpriteRenderer.class);
+        if (sprite != null) {
+            add(sprite);
+        }
+    }
 
+    private void add(SpriteRenderer sprite) {
+        boolean added = false;
+        for (RenderBatch batch : batches) {
+            if (batch.hasRoom()) {
+                batch.addSprite(sprite);
+                added = true;
+                // add to first available batch
+                break;
+            }
+        }
+
+        if (!added) {
+            RenderBatch newBatch = new RenderBatch(MAX_BATCH_SIZE);
+            newBatch.start();
+            batches.add(newBatch);
+            newBatch.addSprite(sprite);
+        }
+    }
+
+    public void render() {
+        for (RenderBatch batch : batches) {
+            batch.render();
+        }
     }
 
 }
