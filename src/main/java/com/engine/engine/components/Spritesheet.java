ed2f366d80590b18288e843890ffd04916eceb72
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2023-11-10 23:49:36 +0100

    add component for spritesheet

---
+++ src/main/java/com/engine/engine/components/Spritesheet.java
@@ -0,0 +1,25 @@
+package com.engine.engine.components;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.engine.engine.renderer.Texture;
+
+public class Spritesheet {
+    private Texture texture;
+    private List<Sprite> sprites;
+
+    public Spritesheet(Texture texture, int spriteWidth, int spriteHeigth, int numSprites, int spacing) {
+        this.sprites = new ArrayList<>();
+        this.texture = texture;
+        int currentX = 0;
+        // top left sprite -> bottom left corner of it
+        int currentY = texture.getHeight() - spriteHeigth;
+        for (int i = 0; i < numSprites; i++) {
+            float topY = (currentY + spriteHeigth) / (float) texture.getHeight();
+            float rightX = (currentX + spriteWidth) / (float) texture.getWidth();
+            float leftX = currentX / (float) texture.getWidth();
+            float bottomY = currentY / (float) texture.getHeight();
+        }
+    }
+}
