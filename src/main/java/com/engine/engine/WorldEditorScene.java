b8955f684d565131ecaa9e80dc5dd0831efa73fc
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2023-11-21 23:36:43 +0100

    test spritesheets

--- src/main/java/com/engine/engine/WorldEditorScene.java
+++ src/main/java/com/engine/engine/WorldEditorScene.java
@@ -5,6 +5,7 @@ import org.joml.Vector4f;
 
 import com.engine.engine.components.Sprite;
 import com.engine.engine.components.SpriteRenderer;
+import com.engine.engine.components.Spritesheet;
 import com.engine.utils.AssetPool;
 
 public class WorldEditorScene extends Scene {
@@ -14,18 +15,35 @@ public class WorldEditorScene extends Scene {
 
     @Override
     public void init() {
+        loadResources();
+
         // System.out.println("create object");
         this.camera = new Camera(new Vector2f(0, 0));
 
-        GameObject sandwich = new GameObject("sandwich", new Transform(new Vector2f(100, 100), new Vector2f(512, 512)));
-        sandwich.addComponent(new SpriteRenderer(new Sprite(AssetPool.getTexture("src/main/assets/images/sandwich.png"))));
-        addGameObjectToScene(sandwich);
+        Spritesheet sprites = AssetPool.getSpritesheet("src/main/assets/spritesheets/spritesheet_16x16.png");
 
-        loadResources();
+        GameObject sandwich = new GameObject("sandwich", new Transform(new Vector2f(100, 100), new Vector2f(300, 300)));
+        sandwich.addComponent(
+                new SpriteRenderer(sprites.getSprite(58)));
+        GameObject sandwich1 = new GameObject("sandwich1",
+                new Transform(new Vector2f(400, 100), new Vector2f(300, 300)));
+        sandwich1.addComponent(
+                new SpriteRenderer(sprites.getSprite(85)));
+        GameObject sandwich2 = new GameObject("sandwich2",
+                new Transform(new Vector2f(700, 100), new Vector2f(300, 300)));
+        sandwich2.addComponent(
+                new SpriteRenderer(sprites.getSprite(66)));
+        addGameObjectToScene(sandwich);
+        addGameObjectToScene(sandwich1);
+        addGameObjectToScene(sandwich2);
     }
 
     private void loadResources() {
         AssetPool.getShader("src/main/assets/shaders/default.glsl");
+
+        AssetPool.addSpritesheet("src/main/assets/spritesheets/spritesheet_16x16.png",
+                new Spritesheet(AssetPool.getTexture("src/main/assets/spritesheets/spritesheet_16x16.png"), 16, 16, 352,
+                        0));
     }
 
     @Override
