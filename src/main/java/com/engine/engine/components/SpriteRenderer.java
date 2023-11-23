658d52c75aa927316d7c7fcc7e1d6b2d40205775
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2023-11-23 23:01:29 +0100

    Add helper methods and isDirty flag

--- src/main/java/com/engine/engine/components/SpriteRenderer.java
+++ src/main/java/com/engine/engine/components/SpriteRenderer.java
@@ -4,11 +4,15 @@ import org.joml.Vector2f;
 import org.joml.Vector4f;
 
 import com.engine.engine.Component;
+import com.engine.engine.Transform;
 import com.engine.engine.renderer.Texture;
 
 public class SpriteRenderer extends Component {
     private Vector4f color;
     private Sprite sprite;
+
+    private boolean isDirty;
+    private Transform lastTransform;
 
     public SpriteRenderer(Vector4f color) {
         this.color = color;
@@ -25,14 +29,27 @@ public class SpriteRenderer extends Component {
 
     @Override
     public void update(float dt) {
+        if (!this.lastTransform.equals(this.gameObject.transform)) {
+            this.gameObject.transform.copy(this.lastTransform);
+            isDirty = true;
+        }
     }
 
     @Override
+    // To provide access to the gameobject
     public void start() {
+        this.lastTransform = gameObject.transform.copy();
     }
 
     public Vector4f getColor() {
         return this.color;
+    }
+
+    public void setColor(Vector4f color) {
+        if (!this.color.equals(color)) {
+            this.color.set(color);
+            this.isDirty = true;
+        }
     }
 
     public Texture getTexture() {
@@ -42,4 +59,10 @@ public class SpriteRenderer extends Component {
     public Vector2f[] getTexCoords() {
         return sprite.getTexCoords();
     }
+
+    public void setSprite(Sprite sprite) {
+        this.sprite = sprite;
+        this.isDirty = true;
+    }
+
 }
