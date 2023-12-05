f0dbcc3c1a6d3e9c33324897cecc7c89b4a272b1
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2023-12-05 21:51:28 +0100

    fix bug

--- src/main/java/com/engine/engine/ImGuiLayer.java
+++ src/main/java/com/engine/engine/ImGuiLayer.java
@@ -192,7 +192,7 @@ public class ImGuiLayer {
 
         // Any Dear ImGui code SHOULD go between ImGui.newFrame()/ImGui.render() methods
         ImGui.newFrame();
-        currentScene.imgui();
+        currentScene.sceneImgui();
         ImGui.showDemoWindow();
         ImGui.render();
 
