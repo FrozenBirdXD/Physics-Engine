ae165f61e9d0e166438a8ef2265e58805039301c
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2023-12-16 23:59:41 +0100

    add load and save methods to scene

--- src/main/java/com/engine/engine/Scene.java
+++ src/main/java/com/engine/engine/Scene.java
@@ -1,9 +1,17 @@
 package com.engine.engine;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
 import com.engine.engine.renderer.Renderer;
+import com.engine.engine.serialization.ComponentDeserializer;
+import com.engine.engine.serialization.GameObjectDeserializer;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import imgui.ImGui;
 
@@ -14,6 +22,8 @@ public abstract class Scene {
     protected List<GameObject> gameObjects = new ArrayList<>();
     // Gameobject that we spectate = has the current focus
     protected GameObject activeGameObject = null;
+
+    protected boolean loadedScene = false;
 
     public Scene() {
         this.renderer = new Renderer();
@@ -61,5 +71,43 @@ public abstract class Scene {
     // To create custom scene integreated Imguis
     public void imgui() {
 
+    }
+
+    public void saveAndExit() {
+        Gson gson = new GsonBuilder()
+                .setPrettyPrinting()
+                .registerTypeAdapter(Component.class, new ComponentDeserializer())
+                .registerTypeAdapter(GameObject.class, new GameObjectDeserializer())
+                .create();
+        try {
+            FileWriter writer = new FileWriter("Scene.txt");
+            writer.write(gson.toJson(this.gameObjects));
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void load() {
+        Gson gson = new GsonBuilder()
+                .setPrettyPrinting()
+                .registerTypeAdapter(Component.class, new ComponentDeserializer())
+                .registerTypeAdapter(GameObject.class, new GameObjectDeserializer())
+                .create();
+        String inFile = "";
+
+        try {
+            inFile = new String(Files.readAllBytes(Paths.get("Scene.txt")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (!inFile.equals("")) {
+            GameObject[] objects = gson.fromJson(inFile, GameObject[].class);
+            for (int i = 0; i < objects.length; i++) {
+                addGameObjectToScene(objects[i]);
+            }
+            this.loadedScene = true;
+        }
     }
 }
