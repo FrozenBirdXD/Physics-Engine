83bee9e45725f87aea3b63236cad9794dafc1f3a
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2023-12-16 23:40:24 +0100

    implement gameobject deserializer

--- src/main/java/com/engine/engine/serialization/GameObjectDeserializer.java
+++ src/main/java/com/engine/engine/serialization/GameObjectDeserializer.java
@@ -2,27 +2,34 @@ package com.engine.engine.serialization;
 
 import java.lang.reflect.Type;
 
+import com.engine.engine.Component;
 import com.engine.engine.GameObject;
+import com.engine.engine.Transform;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
-import com.google.gson.JsonSerializationContext;
-import com.google.gson.JsonSerializer;
 
-public class GameObjectDeserializer implements JsonDeserializer<GameObject>, JsonSerializer<GameObject> {
-    
-    @Override
-    public JsonElement serialize(GameObject src, Type typeOfSrc, JsonSerializationContext context) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'serialize'");
-    }
+public class GameObjectDeserializer implements JsonDeserializer<GameObject> {
 
     @Override
     public GameObject deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
             throws JsonParseException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'deserialize'");
+        JsonObject jsonObject = json.getAsJsonObject();
+        String name = jsonObject.get("name").getAsString();
+        JsonArray components = jsonObject.getAsJsonArray("components");
+        int zIndex = jsonObject.get("zIndex").getAsInt();
+        Transform transform = context.deserialize(jsonObject.get("transform"), Transform.class);
+
+        GameObject gameObject = new GameObject(name, transform, zIndex);
+
+        for (JsonElement element : components) {
+            // component is deserialized using the custom deserializer
+            Component component = context.deserialize(element, Component.class);
+            gameObject.addComponent(component);
+        }
+        return gameObject;
     }
-    
 }
