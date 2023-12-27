f08029c604aee6a32b36d17abd19b26c460e9cce
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2023-12-27 23:57:32 +0100

    use reflection to expose variables

--- src/main/java/com/engine/engine/Component.java
+++ src/main/java/com/engine/engine/Component.java
@@ -1,5 +1,9 @@
 package com.engine.engine;
 
+import java.lang.reflect.Field;
+
+import imgui.ImGui;
+
 public abstract class Component {
     // transient because GameObject already has a list components
     public transient GameObject gameObject = null;
@@ -13,6 +17,25 @@ public abstract class Component {
     }
 
     public void imgui() {
+        try {
+            // get fields from subclass that is running this component
+            Field[] fields = this.getClass().getDeclaredFields();
+            for (Field f : fields) {
+                Class<?> type = f.getType();
+                Object value = f.get(this); // get value contained in that field with reflection
+                String name = f.getName();
 
+                if (type == int.class) {
+                    int castedValue = (int) value;
+                    // because imgui expects ints as arrays
+                    int[] imIntArray = { castedValue };
+                    if (ImGui.dragInt(name + ": ", imIntArray)) {
+                        f.set(this, imIntArray[0]);
+                    }
+                }
+            }
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
 }
