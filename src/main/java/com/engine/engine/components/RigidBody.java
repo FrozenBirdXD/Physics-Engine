7bcb3f231bfe2428d2f3a7a56859ffc0a26f10af
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2023-12-29 00:42:34 +0100

    add rigidbody component

---
+++ src/main/java/com/engine/engine/components/RigidBody.java
@@ -0,0 +1,14 @@
+package com.engine.engine.components;
+
+import org.joml.Vector3f;
+import org.joml.Vector4f;
+
+import com.engine.engine.Component;
+
+public class RigidBody extends Component {
+    private int colliderType = 0;
+    private float friction = 0.8f;
+    private boolean isFalling = false;
+    public Vector3f velocity = new Vector3f(0, 0.5f, 0);
+    public transient Vector4f temporary = new Vector4f(0, 0, 0, 0);
+}
