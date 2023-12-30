f8479ebd2972feac324f422ea9dc6dccb5f14e0c
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2023-12-30 23:52:06 +0100

    inverse matricies

--- src/main/java/com/engine/engine/Camera.java
+++ src/main/java/com/engine/engine/Camera.java
@@ -5,33 +5,49 @@ import org.joml.Vector2f;
 import org.joml.Vector3f;
 
 public class Camera {
-    private Matrix4f projectionMatrix, viewMatrix;
+    private Matrix4f projectionMatrix, viewMatrix, inverseProjectionMatrix, inverseViewMatrix;
     public Vector2f position;
 
     public Camera(Vector2f position) {
         this.position = position;
         this.projectionMatrix = new Matrix4f();
         this.viewMatrix = new Matrix4f();
+        this.inverseProjectionMatrix = new Matrix4f();
+        this.inverseViewMatrix = new Matrix4f();
         adjustProjection();
     }
 
     public void adjustProjection() {
         projectionMatrix.identity();
-        // (0, 1920, 0, 1080, 0.5, 100) as inputs, this means that the camera can "see" any objects that are between x-coordinates 0 and 1920, and y-coordinates 0 and 1080, and z-coordinates 0.5-100
+        // (0, 1920, 0, 1080, 0.5, 100) as inputs, this means that the camera can "see"
+        // any objects that are between x-coordinates 0 and 1920, and y-coordinates 0
+        // and 1080, and z-coordinates 0.5-100
+        
+        // 32.0f * 40.0f is the width of the camera
         projectionMatrix.ortho(0.0f, 32.0f * 40.0f, 0.0f, 32.0f * 21.0f, 0.0f, 100.0f);
+        projectionMatrix.invert(inverseProjectionMatrix);
     }
 
     public Matrix4f getViewMatrix() {
         // Determines where the camera is and where it is looking at
         Vector3f cameraFront = new Vector3f(0.0f, 0.0f, -1.0f);
         Vector3f cameraUp = new Vector3f(0.0f, 1.0f, 0.0f);
-        this.viewMatrix.identity();
-        this.viewMatrix.lookAt(new Vector3f(position.x, position.y, 20.0f),
+        viewMatrix.identity();
+        viewMatrix.lookAt(new Vector3f(position.x, position.y, 20.0f),
                 cameraFront.add(position.x, position.y, 0.0f), cameraUp);
+        viewMatrix.invert(inverseViewMatrix);
         return this.viewMatrix;
     }
 
     public Matrix4f getProjectionMatrix() {
         return this.projectionMatrix;
+    }
+
+    public Matrix4f getInverseViewMatrix() {
+        return this.inverseViewMatrix;
+    }
+
+    public Matrix4f getInverseProjectionMatrix() {
+        return this.inverseProjectionMatrix;
     }
 }
