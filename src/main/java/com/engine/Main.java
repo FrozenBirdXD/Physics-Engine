5f882a379aea48a17404e1233a203401e6836b5d
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2023-12-31 17:11:22 +0100

    use new methods

--- src/main/java/com/engine/Main.java
+++ src/main/java/com/engine/Main.java
@@ -1,6 +1,8 @@
 package com.engine;
 
-import com.engine.engine.Window;
+import com.engine.graphics.Window;
+import com.engine.graphics.WorldEditorScene;
+import com.engine.graphics.utils.Colors;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,8 +10,14 @@ public class Main {
         Window window = Window.get();
 
         window.setTitle("hi");
-        window.setResizable(false);
-        window.setScene(0);
+        window.setResizable(true);
+        // window.setWindowOpacity(0.8f);
+        // window.setWindowBackgroundColor(Colors.Red);
+        // window.setWindowBackgroundColorOpacity(33);
+
+        WorldEditorScene scene = new WorldEditorScene();
+
+        window.setScene(scene);
         window.show();
     }
 }
