a444a247107050f139df4b08b491bad3abcfdc88
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2024-01-10 23:36:09 +0100

    create first shape class line

---
+++ src/main/java/com/engine/graphics/shapes/Line2D.java
@@ -0,0 +1,47 @@
+package com.engine.graphics.shapes;
+
+import org.joml.Vector2f;
+import org.joml.Vector3f;
+
+public class Line2D {
+    private Vector2f from;
+    private Vector2f to;
+    private Vector3f color;
+    private int lifetime;
+
+    public Line2D(Vector2f from, Vector2f to, Vector3f color, int lifetime) {
+        this.from = from;
+        this.to = to;
+        this.color = color;
+        this.lifetime = lifetime;
+    }
+
+    public int beginFrame() {
+        lifetime--;
+        return lifetime;
+    }
+
+    public Vector2f getFrom() {
+        return this.from;
+    }
+
+    public void setFrom(Vector2f from) {
+        this.from = from;
+    }
+
+    public Vector2f getTo() {
+        return this.to;
+    }
+
+    public void setTo(Vector2f to) {
+        this.to = to;
+    }
+
+    public Vector3f getColor() {
+        return this.color;
+    }
+
+    public void setColor(Vector3f color) {
+        this.color = color;
+    }
+}
