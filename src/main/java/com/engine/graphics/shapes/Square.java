aee4d6caf44e04fb22b72a1f70fc3d055c8f87ae
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2024-01-16 23:42:18 +0100

    create Square shape

---
+++ src/main/java/com/engine/graphics/shapes/Square.java
@@ -0,0 +1,13 @@
+package com.engine.graphics.shapes;
+
+import org.joml.Vector4f;
+
+public class Square extends Shape {
+    public Square(String name) {
+        super(name);
+    }
+
+    public Square(String name, Vector4f color) {
+        super(name, color);
+    }
+}
