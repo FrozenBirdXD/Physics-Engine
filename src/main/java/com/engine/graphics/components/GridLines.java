ed7aee6aaba9149d30c01acd53a4821fff13636b
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2024-01-19 23:53:40 +0100

    add component for gridlines

---
+++ src/main/java/com/engine/graphics/components/GridLines.java
@@ -0,0 +1,42 @@
+package com.engine.graphics.components;
+
+import org.joml.Vector2f;
+
+import com.engine.graphics.Window;
+import com.engine.graphics.renderer.DebugDraw;
+import com.engine.graphics.utils.ColorConversion;
+import com.engine.graphics.utils.Colors;
+import com.engine.graphics.utils.Settings;
+
+public class GridLines extends Component {
+    @Override
+    public void update(float dt) {
+        Vector2f cameraPos = Window.get().getScene().getCamera().getPosition();
+        Vector2f projectionSize = Window.get().getScene().getCamera().getProjectionSize();
+
+        int height = (int) projectionSize.y + Settings.GRID_HEIGHT * 2;
+        int width = (int) projectionSize.x + Settings.GRID_WIDTH * 2;
+
+        int firstX = (int) ((cameraPos.x / Settings.GRID_WIDTH) - 1) * Settings.GRID_HEIGHT;
+        int firstY = (int) ((cameraPos.y / Settings.GRID_HEIGHT) - 1) * Settings.GRID_WIDTH;
+        int numLinesVertical = (int) (projectionSize.x / Settings.GRID_WIDTH) + 2;
+        int numLinesHorizontal = (int) (projectionSize.y / Settings.GRID_HEIGHT) + 2;
+
+        int maxLines = Math.max(numLinesHorizontal, numLinesVertical);
+        for (int i = 0; i < maxLines; i++) {
+            int x = firstX + Settings.GRID_WIDTH * i;
+            int y = firstY + Settings.GRID_HEIGHT * i;
+
+            if (i < numLinesVertical) {
+                DebugDraw.addLine2D(new Vector2f(x, firstY), new Vector2f(x, firstY + height),
+                        ColorConversion.colorToRGB(Colors.Black));
+            }
+
+            if (i < numLinesHorizontal) {
+                DebugDraw.addLine2D(new Vector2f(firstX, y), new Vector2f(firstX + width, y),
+                        ColorConversion.colorToRGB(Colors.Black));
+            }
+        }
+
+    }
+}
