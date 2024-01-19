96ad054c8c7ef0502f8b03ac0cf323341e91035d
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2024-01-19 23:54:42 +0100

    update mouse to snap to grid

--- src/main/java/com/engine/graphics/utils/MouseHelper.java
+++ src/main/java/com/engine/graphics/utils/MouseHelper.java
@@ -4,9 +4,10 @@ import static org.lwjgl.glfw.GLFW.GLFW_MOUSE_BUTTON_LEFT;
 
 import com.engine.graphics.GameObject;
 import com.engine.graphics.Window;
+import com.engine.graphics.components.Component;
 import com.engine.graphics.listeners.MouseListener;
 
-public class MouseHelper {
+public class MouseHelper extends Component {
     GameObject holdingGameObject = null;
 
     public void pickupObject(GameObject object) {
@@ -18,10 +19,13 @@ public class MouseHelper {
         holdingGameObject = null;
     }
 
+    @Override
     public void update(float dt) {
         if (holdingGameObject != null) {
-            holdingGameObject.getTransform().setPosition(MouseListener.getOrthoX() - 16,
-                    MouseListener.getOrthoY() - 16);
+            holdingGameObject.getTransform().setPosition(MouseListener.getOrthoX(), MouseListener.getOrthoY());
+            holdingGameObject.getTransform().setPosition(
+                    (int) holdingGameObject.getTransform().getPositionX() / Settings.GRID_WIDTH * Settings.GRID_WIDTH,
+                    (int) holdingGameObject.getTransform().getPositionY() / Settings.GRID_HEIGHT * Settings.GRID_HEIGHT);
 
             if (MouseListener.mouseButtonDown(GLFW_MOUSE_BUTTON_LEFT)) {
                 placeObject();
