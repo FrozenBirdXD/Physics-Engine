d9bec23d59f955fd1f432dba42ff3cdec0e9dddf
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2024-01-31 23:51:02 +0100

    add ellipse class

--- src/main/java/com/engine/graphics/shapes/Ellipse.java
+++ src/main/java/com/engine/graphics/shapes/Ellipse.java
@@ -1,10 +1,48 @@
 package com.engine.graphics.shapes;
 
+import org.joml.Vector2f;
 import org.joml.Vector4f;
 
 public class Ellipse extends Shape {
+    private Vector2f center;
+    private float radiusX;
+    private float radiusY;
+    private int segments;
+    private Vector4f color;
+    private int lifetime;
+    private int lineWidth;
+
+    public Ellipse(Vector2f center, float radiusX, float radiusY, int segments, Vector4f color, int lifetime,
+            int lineWidth) {
+        super("Ellipse", color, lineWidth);
+        this.center = center;
+        this.radiusX = radiusX;
+        this.radiusY = radiusY;
+        this.segments = segments;
+        this.lifetime = lifetime;
+    }
+
     public Ellipse(String name, Vector4f color) {
         super(name, color);
     }
-    
+
+    public Vector2f getCenter() {
+        return center;
+    }
+
+    public float getRadiusX() {
+        return radiusX;
+    }
+
+    public float getRadiusY() {
+        return radiusY;
+    }
+
+    public int getSegments() {
+        return segments;
+    }
+
+    public int getLifetime() {
+        return lifetime;
+    }
 }
